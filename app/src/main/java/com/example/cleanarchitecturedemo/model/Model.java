9e67fe4ecc549f14376117b9c6713446bc04afc9
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-02-17 16:46:08 +0100

    integrate the Oberserver Pattern so whenever the setdata method from the Model.java class is called the screen will changed.

--- app/src/main/java/com/example/cleanarchitecturedemo/model/Model.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/model/Model.java
@@ -1,17 +1,39 @@
 package com.example.cleanarchitecturedemo.model;
+import java.util.ArrayList;
+
 
 public class Model {
 
 
-    private String data = "Hey";
-
+    private String data = "Hello";
+    private final ArrayList<DataObserver> COLLECTIONOFDATAOBSERVERS = new ArrayList<>();
 
     public void setData(String data ){
         this.data = data;
+        notifyObservers();
     }
 
     public String getData(){
         return this.data;
     }
+
+
+
+    private void notifyObservers() {
+        for (DataObserver observer : COLLECTIONOFDATAOBSERVERS) {
+            observer.update();
+        }
+    }
+
+    public void addDataObserver(DataObserver observer) {
+        if (!COLLECTIONOFDATAOBSERVERS.contains(observer))
+            COLLECTIONOFDATAOBSERVERS.add(observer);
+    }
+
+    public interface DataObserver {
+        void update();
+    }
+
+
 
 }
--- app/src/main/java/com/example/cleanarchitecturedemo/view/AndroidView.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/view/AndroidView.java
@@ -8,6 +8,8 @@ import com.example.cleanarchitecturedemo.R;
 import com.example.cleanarchitecturedemo.model.Model;
 
 
+
+
 public class AndroidView extends AppCompatActivity {
 
 
@@ -20,20 +22,39 @@ public class AndroidView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         TextView outputField = findViewById(R.id.textViewScreen);
         outputField.setText(logic.getData());
 
         EditText inputField = findViewById(R.id.editTextViewXml);
         inputField.setText(logic.getData());
+
+
+        // Adding a DataObserver and making it set the outputFiels to what the data.model has.
+        logic.addDataObserver(new Model.DataObserver() {
+            @Override
+            public void update() {
+                String data = logic.getData();
+                TextView outputField = findViewById(R.id.textViewScreen);
+                outputField.setText(data);
+            }
+        });
+
+
+
     }
 
-
+    // Method thats gonna be called when the print Button is pressed.
     public void printEditText(View view) {
         EditText inputField = findViewById(R.id.editTextViewXml);
-        TextView outputField = findViewById(R.id.textViewScreen);
-
         String input = inputField.getText().toString();
         logic.setData(input);
-        outputField.setText(logic.getData());
+
     }
+
+
+
+
+
 }
