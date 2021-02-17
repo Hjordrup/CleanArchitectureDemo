a979787a1c58886fe0c761c9aeb25fda6956eb2f
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-02-17 14:29:29 +0100

    Added a view package and a model package aswell as moved MainActivity.java too the view Package

--- app/src/main/java/com/example/cleanarchitecturedemo/view/MainActivity.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.cleanarchitecturedemo;
+package com.example.cleanarchitecturedemo.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.cleanarchitecturedemo.R;
 
 
 public class MainActivity extends AppCompatActivity {
