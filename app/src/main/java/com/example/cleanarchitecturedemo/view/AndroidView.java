f6857cce1baef81b6202442580cd5413b9c7cf15
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-02-17 14:48:22 +0100

    Get ind and set indput in model class

--- app/src/main/java/com/example/cleanarchitecturedemo/view/AndroidView.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/view/AndroidView.java
@@ -1,28 +1,38 @@
 package com.example.cleanarchitecturedemo.view;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.example.cleanarchitecturedemo.R;
+import com.example.cleanarchitecturedemo.model.Model;
 
 
 public class AndroidView extends AppCompatActivity {
+
+    private Model logic = new Model();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView outputField = findViewById(R.id.textViewScreen);
+        outputField.setText(logic.getData());
+
+        EditText inputField = findViewById(R.id.editTextViewXml);
+        inputField.setText(logic.getData());
     }
 
 
     public void printEditText(View view) {
-        TextView t1 = findViewById(R.id.textViewScreen);
-        EditText e1 = findViewById(R.id.editTextViewXml);
-        String newText = String.valueOf(e1.getText());
-        t1.setText(newText);
+        EditText inputField = findViewById(R.id.editTextViewXml);
+        TextView outputField = findViewById(R.id.textViewScreen);
+
+        String input = inputField.getText().toString();
+        logic.setData(input);
+        outputField.setText(logic.getData());
     }
 }
