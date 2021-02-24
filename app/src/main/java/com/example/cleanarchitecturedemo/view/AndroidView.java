ca9fb9c3493f5cc5e18711026eb484ab6891f073
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-02-24 16:23:36 +0100

    Added the methods need to get LowerCaseViewModel.java to work

--- app/src/main/java/com/example/cleanarchitecturedemo/view/AndroidView.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/view/AndroidView.java
@@ -1,57 +1,48 @@
 package com.example.cleanarchitecturedemo.view;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import com.example.cleanarchitecturedemo.R;
-import com.example.cleanarchitecturedemo.model.Model;
-
-
 
 
 public class AndroidView extends AppCompatActivity {
 
-
-    private Model logic = new Model();
-
-
-
+    private LowerCaseViewModel lowerCaseViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Add observer to view.
+        observeViewModel();
 
         TextView outputField = findViewById(R.id.textViewScreen);
-        outputField.setText(logic.getData());
-
-        EditText inputField = findViewById(R.id.editTextViewXml);
-        inputField.setText(logic.getData());
-
-
-        // Adding a DataObserver and making it set the outputFiels to what the data.model has.
-        logic.addDataObserver(new Model.DataObserver() {
-            @Override
-            public void update() {
-                String data = logic.getData();
-                TextView outputField = findViewById(R.id.textViewScreen);
-                outputField.setText(data);
-            }
-        });
-
-
-
+        outputField.setText(lowerCaseViewModel.getUserInput().getValue());
     }
 
     // Method thats gonna be called when the print Button is pressed.
     public void printEditText(View view) {
         EditText inputField = findViewById(R.id.editTextViewXml);
         String input = inputField.getText().toString();
-        logic.setData(input);
-
+        lowerCaseViewModel.setUserInput(input);
     }
 
-
+    private void observeViewModel(){
+        lowerCaseViewModel = new LowerCaseViewModel();
+        final Observer<String> stringObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                TextView outputField = findViewById(R.id.textViewScreen);
+                outputField.setText(s);
+            }
+        };
+        lowerCaseViewModel.getUserInput().observe(this,stringObserver);
+    }
 }
--- app/src/main/java/com/example/cleanarchitecturedemo/view/LowerCaseViewModel.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/view/LowerCaseViewModel.java
@@ -1,4 +1,39 @@
 package com.example.cleanarchitecturedemo.view;
 
-public class LowerCaseViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.cleanarchitecturedemo.model.Model;
+
+public class LowerCaseViewModel extends ViewModel {
+
+    private final MutableLiveData<String> userInput = new MutableLiveData<>();
+    private Model logic = new Model();
+
+    public LowerCaseViewModel(){
+    observeLogic(logic);
+    userInput.setValue(getLowerCaseInput(logic.getData()));
+    }
+
+    private void observeLogic(final Model logic) {
+        logic.addDataObserver(new Model.DataObserver() {
+            @Override
+            public void update() {
+                String data = logic.getData();
+                userInput.setValue(getLowerCaseInput(data));
+            }
+        });
+    }
+
+    private String getLowerCaseInput(String userInput){
+        return userInput.toLowerCase();
+    }
+
+    public void setUserInput(String userInput){
+        logic.setData(userInput);
+    }
+
+    public MutableLiveData<String> getUserInput() {
+        return userInput;
+    }
 }
